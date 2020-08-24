6e901abce86f6bd9bc272670a15f72a78af30702
lochannimje <65588981+lochannimje@users.noreply.github.com>
2020-08-24 20:21:59 +0530

    Update ViewApplication.java

--- src/main/java/com/travel/view_availability/ViewApplication.java
+++ src/main/java/com/travel/view_availability/ViewApplication.java
@@ -2,13 +2,9 @@ package com.travel.view_availability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@EnableDiscoveryClient
 public class ViewApplication {
 
 	public static void main(String[] args) {
